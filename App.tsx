800b1e2b570d1840696aba37c591f2d1a9023348
Wagner <wagnerweb2010@gmail.com>
2023-06-30 15:57:16 -0300

    fix: Returned the "import React" (that shouldn't have been removed) to files... Oops. Eslint help me on that one.

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ColorPalette } from "./constants";
@@ -6,7 +7,7 @@ import { MainNavigationComponent } from "./pages";
 export default function App() {
   return (
     <SafeAreaView style={{ backgroundColor: ColorPalette.Gray1, flex: 1 }}>
-        <MainNavigationComponent/>
+      <MainNavigationComponent/>
       <StatusBar style="light" backgroundColor={ColorPalette.Primary} />
     </SafeAreaView>
   );
