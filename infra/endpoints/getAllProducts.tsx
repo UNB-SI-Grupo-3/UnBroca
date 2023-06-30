b38ad7f922b8868d0353511369708b7df985baf0
Wagner <wagnerweb2010@gmail.com>
2023-06-30 15:04:39 -0300

    fix: brought code to style

--- infra/endpoints/getAllProducts.tsx
+++ infra/endpoints/getAllProducts.tsx
@@ -1,20 +1,18 @@
 import { backendUrl } from "../../constants";
 
 export const getAllProducts = async () => {
-    try {
-        const response = await fetch(
-            `${backendUrl}/products`,
-        );
+  try {
+    const response = await fetch(`${backendUrl}/products`);
 
-        const json = await response.json();
+    const json = await response.json();
 
-        return json;
-    } catch (error) {
-        console.error(error);
-    }
-  };
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 export async function test() {
-    const result = await getAllProducts();
-    console.log(result);
+  const result = await getAllProducts();
+  console.log(result);
 }
--- infra/endpoints/getProduct.tsx
+++ infra/endpoints/getProduct.tsx
@@ -1,20 +1,18 @@
 import { backendUrl } from "../../constants";
 
-export async function getProduct(id: string){
-    try {
-        const response = await fetch(
-            `${backendUrl}/product?id=${id}`,
-        );
+export async function getProduct(id: string) {
+  try {
+    const response = await fetch(`${backendUrl}/product?id=${id}`);
 
-        const json = await response.json();
+    const json = await response.json();
 
-        return json;
-    } catch (error) {
-        console.error(error);
-    }
-};
+    return json;
+  } catch (error) {
+    console.error(error);
+  }
+}
 
 export async function test() {
-    const result = await getProduct('1');
-    console.log(result);
+  const result = await getProduct("1");
+  console.log(result);
 }
